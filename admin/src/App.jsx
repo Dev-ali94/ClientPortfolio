c860bd04f065858673525caaaec7c86214ac376e
Ali Imran <sheikhaliimran5452@gmail.com>
2025-10-17 21:08:00 +0500

    commit

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,22 +2,23 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Main from './Components/Main'
-import PrivateRoute from './Components/PrivateRoute'   // ✅ Import the guard
 
 const App = () => {
   return (
     <>
       <Routes>
         {/* Public route */}
-        <Route path='/login' element={<Login />} />
+        <Route path='/login' element={
+           
+             <Login />
+          } />
 
         {/* Protected route */}
         <Route 
           path='/' 
           element={
-            <PrivateRoute>
               <Main />
-            </PrivateRoute>
+           
           } 
         />
       </Routes>
